3d4400e27613e077fa1baf4d7ab364eea740c49a
unluckyidiot16 <unluckyidiot16@gmail.com>
2025-11-01 11:29:22 +0900

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,12 @@
 // src/supabaseClient.ts
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+    import.meta.env.VITE_SUPABASE_URL ?? "https://infhrqalyvybktiibtty.supabase.co";
+const supabaseAnonKey =
+    import.meta.env.VITE_SUPABASE_ANON_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZmhycWFseXZ5Ymt0aWlidHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTgzMjIsImV4cCI6MjA3NzQ3NDMyMn0.Skq7wnJBG5EqRVblo-wArAUYjT-3AQbVTRNZJmwWt_E";
 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 let supabase: SupabaseClient;
 
 if (!supabaseUrl || !supabaseAnonKey) {
