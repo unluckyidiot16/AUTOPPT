25b5f052470fe4fbb0d31903b3fc80db2f009ae0
unluckyidiot16 <unluckyidiot16@gmail.com>
2025-11-01 11:17:16 +0900

    adjust

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/AUTOPPT/", // <= 레포 이름
     server: {
         host: true,
         port: 5173,
